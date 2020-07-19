df1ff736fc356366b9d6a6af2418c30a152a9240
Vincent Le Badezet <vincent.l@lajavaness.com>
2020-07-19 19:52:47 +0200

    feat: Add bcp-47 language list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,46 @@ import React, { useState } from 'react'
 
 import Vocal, { isSupported } from '@untemps/react-vocal'
 
+const LANGS = {
+	'ar-SA': 'Arabic Saudi Arabia',
+	'cs-CZ': 'Czech Czech Republic',
+	'da-DK': 'Danish Denmark',
+	'de-DE': 'German Germany',
+	'el-GR': 'Modern Greek Greece',
+	'en-AU': 'English Australia',
+	'en-GB': 'English United Kingdom',
+	'en-IE': 'English Ireland',
+	'en-US': 'English United States',
+	'en-ZA': 'English South Africa',
+	'es-ES': 'Spanish Spain',
+	'es-MX': 'Spanish Mexico',
+	'fi-FI': 'Finnish Finland',
+	'fr-CA': 'French Canada',
+	'fr-FR': 'French France',
+	'he-IL': 'Hebrew Israel',
+	'hi-IN': 'Hindi India',
+	'hu-HU': 'Hungarian Hungary',
+	'id-ID': 'Indonesian Indonesia',
+	'it-IT': 'Italian Italy',
+	'ja-JP': 'Japanese Japan',
+	'ko-KR': 'Korean Republic of Korea',
+	'nl-BE': 'Dutch Belgium',
+	'nl-NL': 'Dutch Netherlands',
+	'no-NO': 'Norwegian Norway',
+	'pl-PL': 'Polish Poland',
+	'pt-BR': 'Portuguese Brazil',
+	'pt-PT': 'Portuguese Portugal',
+	'ro-RO': 'Romanian Romania',
+	'ru-RU': 'Russian Russian Federation',
+	'sk-SK': 'Slovak Slovakia',
+	'sv-SE': 'Swedish Sweden',
+	'th-TH': 'Thai Thailand',
+	'tr-TR': 'Turkish Turkey',
+	'zh-CN': 'Chinese China',
+	'zh-HK': 'Chinese Hong Kong',
+	'zh-TW': 'Chinese Taiwan',
+}
+
 const App = () => {
 	const [result, setResult] = useState('')
 	const [lang, setLang] = useState('en-US')
@@ -66,8 +106,9 @@ const App = () => {
 						onChange={_onLangChange}
 						className="font-display text-gray-700 leading-tight appearance-none border rounded-full w-full py-6 pl-6 pr-12 focus:outline-none focus:shadow-outline"
 					>
-						<option>en-US</option>
-						<option>fr-FR</option>
+						{Object.keys(LANGS).map((lang) => (
+							<option>{lang}</option>
+						))}
 					</select>
 					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-6 text-gray-700">
 						<svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
