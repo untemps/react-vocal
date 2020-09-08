db1aafc6996be36e4982f85a10df1f273c70eb3d
Vincent Le Badezet <vincent@lajavaness.com>
2020-09-08 05:56:32 +0200

    refactor: Remove all styles from dev example

--- dev/src/index.js
+++ dev/src/index.js
@@ -5,81 +5,38 @@ import ReactDOM from 'react-dom'
 import Vocal from '../../src'
 
 const App = () => {
-	const [result, setResult] = useState('')
 	const [logs, setLogs] = useState('')
 
+	const _log = (value) => setLogs((logs) => `${logs}${logs.length > 0 ? '\n' : ''} ----- ${value}`)
+
 	const _onVocalStart = () => {
-		setResult('')
-		setLogs((logs) => `${logs}\nstart`)
+		_log('start')
 	}
 
 	const _onVocalEnd = () => {
-		setLogs((logs) => `${logs}\nend`)
+		_log(`end`)
 	}
 
 	const _onVocalResult = (result) => {
-		setResult(result)
-		setLogs((logs) => `${logs}\nresult: "${result}"`)
+		_log(`result: "${result}"`)
 	}
 
 	const _onVocalError = (e) => {
-		setLogs((logs) => `${logs}\n${e.message}`)
+		_log(e.message)
 	}
 
 	return (
-		<div
-			style={{
-				backgroundColor: '#eeeeee',
-				width: '100%',
-				height: '100%',
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				flexDirection: 'column',
-			}}
-		>
-			<div style={{ position: 'relative' }}>
-				<Vocal
-					onStart={_onVocalStart}
-					onEnd={_onVocalEnd}
-					onResult={_onVocalResult}
-					onError={_onVocalError}
-					style={{ position: 'absolute', right: 6, top: 16 }}
-				/>
-				<input
-					defaultValue={result}
-					style={{
-						width: 300,
-						height: 50,
-						paddingLeft: 16,
-						paddingRight: 32,
-						backgroundColor: 'white',
-						border: 'none',
-						boxShadow: '10px 10px 5px -6px rgba(0,0,0,0.2)',
-					}}
-				/>
-			</div>
-			<div style={{ display: 'flex', flexDirection: 'column', marginTop: 32 }}>
-				<label htmlFor="logsarea">Logs:</label>
-				<textarea
-					id="logsarea"
-					value={logs}
-					rows={12}
-					disabled
-					style={{
-						width: 300,
-						paddingLeft: 16,
-						paddingRight: 32,
-						marginTop: 8,
-						backgroundColor: 'white',
-						border: 'none',
-						boxShadow: '10px 10px 5px -6px rgba(0,0,0,0.2)',
-						resize: 'none',
-						outline: 'none',
-					}}
-				/>
-			</div>
-		</div>
+		<>
+			<Vocal onStart={_onVocalStart} onEnd={_onVocalEnd} onResult={_onVocalResult} onError={_onVocalError}>
+				{(start, stop) => (
+					<div>
+						<button onClick={start}>Start</button>
+						<button onClick={stop}>Stop</button>
+					</div>
+				)}
+			</Vocal>
+			<textarea value={logs} rows={30} disabled style={{ width: '100%', marginTop: 16 }} />
+		</>
 	)
 }
 
