9a88423a10590d447552584e8e945b5164e65d69
untemps <6Mv2^2S6$2>
2021-07-05 19:36:47 +0200

    feat: Add precision parameter to useCommands hook

--- src/hooks/useCommands.js
+++ src/hooks/useCommands.js
@@ -1,13 +1,13 @@
 import Fuse from 'fuse.js'
 
-const useCommands = (commands) => {
+const useCommands = (commands, precision = 0.4) => {
 	commands = !!commands
 		? Object.entries(commands)?.reduce((acc, [key, value]) => ({ [key.toLocaleLowerCase()]: value }), {})
 		: {}
 
 	const triggerCommand = (input) => {
 		const fuse = new Fuse(Object.keys(commands), { includeScore: true, ignoreLocation: true })
-		const result = fuse.search(input).filter((r) => r.score < 0.4)
+		const result = fuse.search(input).filter((r) => r.score < precision)
 		if (!!result?.length) {
 			const key = result[0].item.toLocaleLowerCase()
 			return commands[key]?.(result)
