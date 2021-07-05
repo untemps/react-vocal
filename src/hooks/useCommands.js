32b0cfc9b25987aaf53e1b2bb3a230dd4bec6669
untemps <6Mv2^2S6$2>
2021-07-05 07:52:35 +0200

    fix: Fix issue when undefined commands are passed to useCommands hook

--- src/hooks/useCommands.js
+++ src/hooks/useCommands.js
@@ -1,14 +1,16 @@
 import Fuse from 'fuse.js'
 
 const useCommands = (commands) => {
-	commands = Object.entries(commands).reduce((acc, [key, value]) => ({ [key.toLocaleLowerCase()]: value }), {})
+	commands = !!commands
+		? Object.entries(commands)?.reduce((acc, [key, value]) => ({ [key.toLocaleLowerCase()]: value }), {})
+		: {}
 
 	const triggerCommand = (command) => {
 		const fuse = new Fuse(Object.keys(commands), { includeScore: true, ignoreLocation: true })
 		const result = fuse.search(command).filter((r) => r.score < 0.4)
 		if (!!result?.length) {
 			const key = result[0].item.toLocaleLowerCase()
-			return commands?.[key]?.(result)
+			return commands[key]?.(result)
 		}
 		return null
 	}
