4f9b9347cc076066d542f7d948fed8f0b586342b
Vincent Le Badezet <v.lebadezet@untemps.net>
2026-05-24 19:27:39 +0200

    chore: Configure coverage thresholds in vitest (#153)

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,12 @@ export default defineConfig({
 			provider: 'v8',
 			reporter: ['text', 'lcov'],
 			reportsDirectory: './coverage',
+			thresholds: {
+				lines: 90,
+				functions: 90,
+				branches: 85,
+				statements: 90,
+			},
 		},
 	},
 })
